02f146c73eee6dd0014ef71c2a34dd1cfdbce9a7
Yanwei Liu <e96031413@hotmail.com>
2019-01-01 23:22:58 +0800

    PyAutoGUI

---
+++ 高師大課表自動截圖/Auto-Class-Crawler-2.py
@@ -0,0 +1,194 @@
+###上學期(向下滑動)
+import webbrowser
+import pyautogui
+#先做上學期的
+webbrowser.open('https://sso.nknu.edu.tw/Stu/Day/inEnroll/scheduleDepartment.aspx')
+
+#pyautogui.click(x=287, y=588, duration=4) #選擇下學期
+pyautogui.click(x=282, y=651, duration=5)
+pyautogui.click(x=1042, y=522, duration=2)
+
+
+#移動滾輪
+pyautogui.moveTo(1237, 575,duration=2)
+pyautogui.click(1237, 575,duration=2)
+pyautogui.dragTo(1239, 801,2, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=552, duration=2)
+
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動滾輪
+pyautogui.moveTo(1237, 518,duration=2)
+pyautogui.click(1237, 518,duration=2)
+
+pyautogui.dragTo(1239, 801,2, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=552, duration=2)
+pyautogui.click(1033, 583,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動滾輪
+pyautogui.moveTo(1237, 575)
+pyautogui.click(1237, 575)
+pyautogui.dragTo(1239, 801, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=552, duration=2)
+pyautogui.click(1033, 613,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動滾輪
+pyautogui.moveTo(1237, 575)
+pyautogui.click(1237, 575)
+pyautogui.dragTo(1239, 801, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=552, duration=2)
+pyautogui.click(1033, 643,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動滾輪
+pyautogui.moveTo(1237, 575)
+pyautogui.click(1237, 575)
+pyautogui.dragTo(1239, 801, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=552, duration=2)
+pyautogui.click(1033, 673,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動滾輪
+pyautogui.moveTo(1237, 575)
+pyautogui.click(1237, 575)
+pyautogui.dragTo(1239, 801, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=552, duration=2)
+pyautogui.click(1033, 703,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動滾輪
+pyautogui.moveTo(1237, 575)
+pyautogui.click(1237, 575)
+pyautogui.dragTo(1239, 801, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=552, duration=2)
+pyautogui.click(1033, 733,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動滾輪
+pyautogui.moveTo(1237, 575)
+pyautogui.click(1237, 575)
+pyautogui.dragTo(1239, 801, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=552, duration=2)
+pyautogui.click(1033, 763,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動滾輪
+pyautogui.moveTo(1237, 575)
+pyautogui.click(1237, 575)
+pyautogui.dragTo(1239, 801, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=552, duration=2)
+pyautogui.click(1033, 793,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動滾輪
+pyautogui.moveTo(1237, 575)
+pyautogui.click(1237, 575)
+pyautogui.dragTo(1239, 801, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=552, duration=2)
+pyautogui.click(1033, 823,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動滾輪
+pyautogui.moveTo(1237, 575)
+pyautogui.click(1237, 575)
+pyautogui.dragTo(1239, 801, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=552, duration=2)
+pyautogui.click(1033, 853,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動滾輪
+pyautogui.moveTo(1237, 575)
+pyautogui.click(1237, 575)
+pyautogui.dragTo(1239, 801, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=552, duration=2)
+pyautogui.click(1033, 883,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動滾輪
+pyautogui.moveTo(1237, 575)
+pyautogui.click(1237, 575)
+pyautogui.dragTo(1239, 801, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=552, duration=2)
+pyautogui.click(1033, 913,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
---
+++ 高師大課表自動截圖/Auto-Class-Crawler.py
@@ -0,0 +1,311 @@
+###上學期(尚未向下滑動)
+
+import webbrowser
+import pyautogui
+#先做上學期的
+webbrowser.open('https://sso.nknu.edu.tw/Stu/Day/inEnroll/scheduleDepartment.aspx')
+
+#pyautogui.click(x=287, y=588, duration=4) #選擇下學期
+pyautogui.click(x=282, y=651, duration=5)
+pyautogui.click(x=1042, y=522, duration=2)
+
+pyautogui.click(1033, 592,duration=2)  #換系所1
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(814, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動到不同系所
+pyautogui.click(x=1042, y=522, duration=2)
+pyautogui.click(1033, 629,duration=2)  #換系所2
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(814, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動到不同系所
+pyautogui.click(x=1042, y=522, duration=2)
+pyautogui.click(1033, 660,duration=2)  #換系所3
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(814, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動到不同系所
+pyautogui.click(x=1042, y=522, duration=2)
+pyautogui.click(1033, 750,duration=2)  #換系所4
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動到不同系所
+pyautogui.click(x=1042, y=522, duration=2)
+pyautogui.click(1033, 780,duration=2)  #換系所5
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動到不同系所
+pyautogui.click(x=1042, y=522, duration=2)
+pyautogui.click(1033, 810,duration=3)  #換系所6
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動到不同系所
+pyautogui.click(x=1042, y=522, duration=2)
+pyautogui.click(1033, 840,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動到不同系所
+pyautogui.click(x=1042, y=522, duration=2)
+pyautogui.click(1033, 870,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動到不同系所
+pyautogui.click(x=1042, y=522, duration=2)
+pyautogui.click(1033, 900,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動到不同系所
+pyautogui.click(x=1042, y=522, duration=2)
+pyautogui.click(1033, 930,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動到不同系所
+pyautogui.click(x=1042, y=522, duration=2)
+pyautogui.click(1033, 960,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動到不同系所
+pyautogui.click(x=1042, y=522, duration=2)
+pyautogui.click(1033, 990,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動滾輪
+pyautogui.moveTo(1237, 575)
+pyautogui.click(1237, 575)
+pyautogui.dragTo(1239, 852, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=522, duration=2)
+pyautogui.click(1033, 553,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動滾輪
+pyautogui.moveTo(1237, 575)
+pyautogui.click(1237, 575)
+pyautogui.dragTo(1239, 852, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=522, duration=2)
+pyautogui.click(1033, 583,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動滾輪
+pyautogui.moveTo(1237, 575)
+pyautogui.click(1237, 575)
+pyautogui.dragTo(1239, 852, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=522, duration=2)
+pyautogui.click(1033, 613,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動滾輪
+pyautogui.moveTo(1237, 575)
+pyautogui.click(1237, 575)
+pyautogui.dragTo(1239, 852, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=522, duration=2)
+pyautogui.click(1033, 643,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動滾輪
+pyautogui.moveTo(1237, 575)
+pyautogui.click(1237, 575)
+pyautogui.dragTo(1239, 852, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=522, duration=2)
+pyautogui.click(1033, 673,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動滾輪
+pyautogui.moveTo(1237, 575)
+pyautogui.click(1237, 575)
+pyautogui.dragTo(1239, 852, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=522, duration=2)
+pyautogui.click(1033, 703,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動滾輪
+pyautogui.moveTo(1237, 575)
+pyautogui.click(1237, 575)
+pyautogui.dragTo(1239, 852, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=522, duration=2)
+pyautogui.click(1033, 733,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動滾輪
+pyautogui.moveTo(1237, 575)
+pyautogui.click(1237, 575)
+pyautogui.dragTo(1239, 852, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=522, duration=2)
+pyautogui.click(1033, 763,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動滾輪
+pyautogui.moveTo(1237, 575)
+pyautogui.click(1237, 575)
+pyautogui.dragTo(1239, 852, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=522, duration=2)
+pyautogui.click(1033, 793,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動滾輪
+pyautogui.moveTo(1237, 575)
+pyautogui.click(1237, 575)
+pyautogui.dragTo(1239, 852, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=522, duration=2)
+pyautogui.click(1033, 823,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動滾輪
+pyautogui.moveTo(1237, 575)
+pyautogui.click(1237, 575)
+pyautogui.dragTo(1239, 852, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=522, duration=2)
+pyautogui.click(1033, 853,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動滾輪
+pyautogui.moveTo(1237, 575)
+pyautogui.click(1237, 575)
+pyautogui.dragTo(1239, 852, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=522, duration=2)
+pyautogui.click(1033, 883,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
+#移動滾輪
+pyautogui.moveTo(1237, 575)
+pyautogui.click(1237, 575)
+pyautogui.dragTo(1239, 852, button='left')
+#移動到不同系所
+pyautogui.click(x=1042, y=522, duration=2)
+pyautogui.click(1033, 913,duration=2)  #換系所
+pyautogui.click(1601, 584,duration=2)
+pyautogui.click(1528, 59,duration=2)
+pyautogui.click(1295, 277,duration=2)
+pyautogui.click(1502, 176,duration=2)
+pyautogui.click(1087, 248,duration=2)
+pyautogui.click(866, 244,duration=2)
+pyautogui.click(557, 546,duration=2)
