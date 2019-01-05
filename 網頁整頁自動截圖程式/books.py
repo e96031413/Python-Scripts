1b3bc650355b5e6f6a3d3daaf39f7b829d0d867b
Yanwei Liu <e96031413@hotmail.com>
2019-01-05 17:59:03 +0800

    added

--- 網頁整頁自動截圖程式/books.py
+++ 網頁整頁自動截圖程式/books.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 import time
-url='https://search.books.com.tw/search/query/cat/1/qsub/001/key/python/sort/5/c/0'
+url='https://search.books.com.tw/search/query/cat/1/qsub/001/key/javascript/sort/5/c/0'
 
 browser = webdriver.Firefox()
 browser.set_window_size(1200,4500)
