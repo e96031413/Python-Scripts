4a178ebe7fa9265662127eb0a4cb85c8fddeb3d8
e96031413 <30921855+e96031413@users.noreply.github.com>
2019-05-01 22:48:56 +0800

    added note

--- FreeProxy.py
+++ FreeProxy.py
@@ -1,6 +1,8 @@
+#自動抓取可用的Proxy
+#可指定每次抓取Proxy數量(num_proxies為Proxy數量)
+
 from FreeProxy import ProxyTool
 pt = ProxyTool.ProxyTool()
 proxies = pt.getProxy(num_proxies=10, max_tries=5)
 print(proxies)
 
-#num_proxies為Proxy數量
