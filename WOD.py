e103e57eaaf0d6a18aa8c92f5e90c6ba9d9d851d
e96031413 <30921855+e96031413@users.noreply.github.com>
2019-05-21 21:53:13 +0800

    added

--- WOD.py
+++ WOD.py
@@ -2,41 +2,41 @@ import requests
 import random
 from bs4 import BeautifulSoup
 
-page = random.randint(1,636)
+page = random.randint(1, 636)
 page = str(page)
-print('The Page is:',page+'\n')
-resp = requests.get('http://wod-gen.com/exercise/'+page)
+print('The Page is:', page + '\n')
+resp = requests.get('http://wod-gen.com/exercise/' + page)
 soup = BeautifulSoup(resp.text, 'html.parser')
-name=soup.find_all('h2')[0].get_text()
+name = soup.find_all('h2')[0].get_text()
 print(name)
 
 try:
-	time=soup.find_all('h2')[1].get_text()
-	print(time)
+    time = soup.find_all('h2')[1].get_text()
+    print(time)
 except:
-	pass
+    pass
 
-workout=soup.find_all('ul')[0].get_text()
+workout = soup.find_all('ul')[0].get_text()
 print(workout)
 
-#with open('TodaysWorkout.txt','w') as f:
-	#for exercise in workout:
-		#f.write(workout)
+# with open('TodaysWorkout.txt','w') as f:
+# for exercise in workout:
+# f.write(workout)
 try:
-	description=soup.find_all('h3')[0].get_text()
-	print(description)
+    description = soup.find_all('h3')[0].get_text()
+    print(description)
 
-	#with open('TodaysWorkout.txt','w') as f:
-		#for detail in description:
-			#f.write(detail)
+    # with open('TodaysWorkout.txt','w') as f:
+    # for detail in description:
+    # f.write(detail)
 except:
-	pass
+    pass
 
-with open('TodaysWorkout.txt','w') as f:
-	try:
-		f.write(name+'\n')
-		f.write(time+'\n')
-		f.write(workout+'\n')
-		f.write(description+'\n')
-	except:
-		pass
+with open('TodaysWorkout.txt', 'w') as f:
+    try:
+        f.write(name + '\n')
+        f.write(time + '\n')
+        f.write(workout + '\n')
+        f.write(description + '\n')
+    except:
+        pass
--- 高師大圖書館爬蟲/library.py
+++ 高師大圖書館爬蟲/library.py
@@ -1,18 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
 
-with open('list.txt') as url:
+with open('eng-list.txt') as url:
     for urls in url:
-        resp = requests.get(urls, headers=headers).text 
+        resp = requests.get(urls, headers=headers).text
         soup = BeautifulSoup(resp, 'lxml')
         try:
-        	for i in range(0,50):
-        		bookName=soup.find_all(class_="briefcitTitle")[i]
-        		content=bookName.text
-        		print(content)
+            for i in range(0, 50):
+                bookName = soup.find_all(class_="briefcitTitle")[i]
+                content = bookName.text
+                print(content)
         except:
-        	pass
-
-
-
+            pass
