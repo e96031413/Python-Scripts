d3a49bc82155920071670c2b8d65502bb17e9d83
e96031413 <30921855+e96031413@users.noreply.github.com>
2019-06-26 22:25:48 +0800

    added apk.tw

--- 高師大圖書館爬蟲/library.py
+++ 高師大圖書館爬蟲/library.py
@@ -1,3 +1,5 @@
+'''
+#single processing in 25.4s
 import requests
 from bs4 import BeautifulSoup
 headers = {
@@ -14,3 +16,25 @@ with open('eng-list.txt') as url:
                 print(content)
         except:
             pass
+'''
+#multiprocessing in 22.6s    
+
+import requests
+from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
+
+with ThreadPoolExecutor(128) as executor:
+    with open('eng-list.txt') as url:
+        for urls in url:
+            resp = requests.get(urls, headers=headers).text
+            soup = BeautifulSoup(resp, 'lxml')
+        try:
+            for i in range(0, 50):
+                bookName = soup.find_all(class_="briefcitTitle")[i]
+                content = bookName.text
+                print(content)
+        except:
+            pass
+        result=executor.map(content,urls)
