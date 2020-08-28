95d15dc0f2083837868c3862b53bbc67980fcb59
e96031413 <30921855+e96031413@users.noreply.github.com>
2020-08-28 15:21:28 +0800

    美股指數抓取

---
+++ 美股指數抓取機器人/us-stock.bat
@@ -0,0 +1,2 @@
+C:\Users\e96031413\Anaconda3\python.exe "C:\Users\e96031413\us-stock\us-stock.py"
+exit
---
+++ 美股指數抓取機器人/us-stock.py
@@ -0,0 +1,63 @@
+import sys
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import requests
+
+
+driver_path = r"C:\Users\e96031413\chromedriver.exe"
+url ="https://www.cnyes.com/usstock"
+options = Options()
+
+prefs = {
+    'profile.default_content_setting_values' :
+        {
+        'notifications' : 2
+         }
+}
+options.add_experimental_option('prefs',prefs)
+options.add_argument("--headless")            #不開啟實體瀏覽器背景執行
+
+
+
+browser=webdriver.Chrome(driver_path,options=options)
+browser.get(url)
+
+DJI = browser.find_element_by_xpath("/html/body/div[1]/div/div[3]/div[5]/div/section[2]/div[2]/div/div/table/tbody/tr[1]/td[2]/a").text
+DJI_price = browser.find_element_by_xpath("/html/body/div[1]/div/div[3]/div[5]/div/section[2]/div[2]/div/div/table/tbody/tr[1]/td[3]/span").text
+DJI_scale_price = browser.find_element_by_xpath("/html/body/div[1]/div/div[3]/div[5]/div/section[2]/div[2]/div/div/table/tbody/tr[1]/td[4]/span").text
+DJI_scale = browser.find_element_by_xpath("/html/body/div[1]/div/div[3]/div[5]/div/section[2]/div[2]/div/div/table/tbody/tr[1]/td[5]/span").text
+
+SP500 = browser.find_element_by_xpath("/html/body/div[1]/div/div[3]/div[5]/div/section[2]/div[2]/div/div/table/tbody/tr[2]/td[2]/a").text
+SP500_price = browser.find_element_by_xpath("/html/body/div[1]/div/div[3]/div[5]/div/section[2]/div[2]/div/div/table/tbody/tr[2]/td[3]/span").text
+SP500_scale_price = browser.find_element_by_xpath("/html/body/div[1]/div/div[3]/div[5]/div/section[2]/div[2]/div/div/table/tbody/tr[2]/td[4]/span").text
+SP500_scale = browser.find_element_by_xpath("/html/body/div[1]/div/div[3]/div[5]/div/section[2]/div[2]/div/div/table/tbody/tr[2]/td[5]/span").text
+
+SOX = browser.find_element_by_xpath("/html/body/div[1]/div/div[3]/div[5]/div/section[2]/div[2]/div/div/table/tbody/tr[3]/td[2]/a").text
+SOX_price = browser.find_element_by_xpath("/html/body/div[1]/div/div[3]/div[5]/div/section[2]/div[2]/div/div/table/tbody/tr[3]/td[3]/span").text
+SOX_scale_price = browser.find_element_by_xpath("/html/body/div[1]/div/div[3]/div[5]/div/section[2]/div[2]/div/div/table/tbody/tr[2]/td[4]/span").text
+SOX_scale = browser.find_element_by_xpath("/html/body/div[1]/div/div[3]/div[5]/div/section[2]/div[2]/div/div/table/tbody/tr[3]/td[5]/span").text
+
+NASDAQ = browser.find_element_by_xpath("/html/body/div[1]/div/div[3]/div[5]/div/section[2]/div[2]/div/div/table/tbody/tr[4]/td[2]/a").text
+NASDAQ_price = browser.find_element_by_xpath("/html/body/div[1]/div/div[3]/div[5]/div/section[2]/div[2]/div/div/table/tbody/tr[4]/td[3]/span").text
+NASDAQ_scale_price = browser.find_element_by_xpath("/html/body/div[1]/div/div[3]/div[5]/div/section[2]/div[2]/div/div/table/tbody/tr[4]/td[4]").text
+NASDAQ_scale = browser.find_element_by_xpath("/html/body/div[1]/div/div[3]/div[5]/div/section[2]/div[2]/div/div/table/tbody/tr[4]/td[5]").text
+
+content = "\n\n" + DJI + "  ,"+DJI_price + "  ," + DJI_scale_price +  "  ,"+ DJI_scale + "\n\n" + SP500 + "  ,"+ SP500_price + "  ,"+ SP500_scale_price + "  ,"+ SP500_scale + "\n\n" + SOX + "  ,"+ SOX_price + "  ,"+ SOX_scale_price + "  ,"+ SOX_scale + "\n\n" + NASDAQ + "  ," + NASDAQ_price + "  ," + NASDAQ_scale_price + "  ," + NASDAQ_scale
+browser.close()
+
+def lineNotifyMessage(token, msg):
+    headers = {
+          "Authorization": "Bearer " + token, 
+          "Content-Type" : "application/x-www-form-urlencoded"
+      }
+    payload = {'message': msg}
+    r = requests.post("https://notify-api.line.me/api/notify", headers = headers, params = payload)
+    return r.status_code
+  # 修改為你要傳送的訊息內容
+  # 修改為你的權杖內容
+token = 'tJD6Z8CharNwEPyAVa5FoMB1JBbN2S4Q9HSuyNtaXXl'
+lineNotifyMessage(token, content)
+browser.quit()
+sys.exit()
+
